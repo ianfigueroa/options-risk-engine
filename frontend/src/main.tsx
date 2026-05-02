afaaece2a9da93c6879b1cfe2a6d149422f64487
ItzTheLegend <ianfigueroa12345@gmail.com>
2026-05-01 20:37:00 -0400

    style the trading desk layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './dashboard.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
